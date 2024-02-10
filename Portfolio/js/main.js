01a26fd1c726d5713ec00f8a1ecb20f412542c34
YamaMaru15 <liberte1la1vie7@gmail.com>
2024-02-10 10:46:41 +0900

    都道府県訪問記録アプリの情報追加、一部文言修正

---
+++ Portfolio/js/main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+{
+  //hamburger menu
+  $('#hamburger').on('click', function(){
+      $('.header-list__title-humberger').toggleClass('humberger__close');
+      $('.header-list__wrap-popup').slideToggle();
+  });
+
+  // popup title&text SET 
+  $('.portfolio__col--yamamaru-img').click(function() {
+    $('.portfolio__popup--overlay, .portfolio__popup--yamamaru-content').fadeIn();
+    $('.header').fadeOut();
+  });
+
+  $('.portfolio__col--typing-img').click(function() {
+    $('.portfolio__popup--overlay, .portfolio__popup--typing-content').fadeIn();
+    $('.header').fadeOut();
+  });
+
+  $('.portfolio__col--memo-img').click(function() {
+    $('.portfolio__popup--overlay, .portfolio__popup--memo-content').fadeIn();
+    $('.header').fadeOut();
+  });
+
+  $('.portfolio__col--prefectures-img').click(function() {
+    $('.portfolio__popup--overlay, .portfolio__popup--prefectures-content').fadeIn();
+    $('.header').fadeOut();
+  });
+
+  $('.portfolio__pop--close-btn, .portfolio__popup--overlay').click(function() {
+    $('.portfolio__popup--overlay, .portfolio__popup--content').fadeOut();
+    $('.header').fadeIn();
+  });
+
+  // fadeUp Animation
+  function fadeUpAnime(){
+    $('.fadeUp').each(function() {
+      var elemPos = $(this).offset().top-50;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+        $(this).addClass('fadeUpAnime');
+      }else{
+        $(this).removeClass('fadeUpAnime');
+      }
+    });
+  }
+
+  $(window).scroll(function (){
+    fadeUpAnime()
+  });
+
+  $(window).on('load', function(){
+    fadeUpAnime();
+  });
+}
